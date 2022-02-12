3e8c033bfe2f42a9268559b3923bfe37eeb96acf
makaracc <makara.chhaing@gmail.com>
2022-02-12 18:06:30 +1100

    boxes

--- src/component/box-grid.tsx
+++ src/component/box-grid.tsx
@@ -10,14 +10,17 @@ interface Props {
 type BoxType = "start" | "wall" | "end";
 
 interface BoxOfGrid {
+  // [index: number]: {
   key: string;
   element: JSX.Element;
+  // };
 }
 
 export const BoxGrid: React.FC<Props> = (props) => {
   const rows: BoxOfGrid[] = [];
   const columns = [];
-  const gridOfBoxes: any[] = [];
+  // const gridOfBoxes: any[] = [];
+  const gridOfBoxes: Array<BoxOfGrid>[] = [[]];
   const [start, setStart] = React.useState({});
   const [wall, setWall] = React.useState({});
   const [goal, setGoal] = React.useState({});
@@ -26,31 +29,37 @@ export const BoxGrid: React.FC<Props> = (props) => {
     console.log("box clicked");
   };
 
+  // for (let i = 0; i < props.row; i++) {
+  //   gridOfBoxes[i] = [];
+  //   for (let j = 0; j < props.column; j++) {
+  //     const val = { key: `${i}${j}`, element: <SmallBox /> } as BoxOfGrid;
+  //     gridOfBoxes[i].push(val);
+  //   }
+  // }
   for (let i = 0; i < props.row; i++) {
     gridOfBoxes[i] = [];
     for (let j = 0; j < props.column; j++) {
       const val = { key: `${i}${j}`, element: <SmallBox /> } as BoxOfGrid;
-      gridOfBoxes[i].push(val);
+      gridOfBoxes[i][j] = val;
     }
   }
 
-  console.log(gridOfBoxes);
-  console.log(<SmallBox />);
-
   const component = (
-    <Grid
-      container
-      justifyContent="center"
-      alignItems="center"
-      direction="column"
-    >
-      {gridOfBoxes.map((boxes, i) => (
-        <Grid item container direction="row" key={boxes[i].key}>
-          {/* {boxes.map((box: any) => (
-            <Grid item key={`element ${boxes.key}`}>
+    <Grid container direction="column">
+      {gridOfBoxes.map((boxes: BoxOfGrid[], i) => (
+        <Grid
+          item
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          key={`row${i + Math.random() * 20}`}
+        >
+          {boxes.map((box) => (
+            <Grid item key={`element ${box.key}`}>
               {box.element}
             </Grid>
-          ))} */}
+          ))}
         </Grid>
       ))}
     </Grid>
